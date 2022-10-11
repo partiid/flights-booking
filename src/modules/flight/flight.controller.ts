9374793ce3d5b934fb67b8c3ba200fb837855daf
partiid <sarkowag@gmail.com>
2022-10-11 23:10:26 +0200

    db seeding for aiports, countries and cities

--- src/modules/flight/flight.controller.ts
+++ src/modules/flight/flight.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { FlightService } from '../../shared/services/flight.service';
+import { DataGenerator } from '../../classes/dataGenerator';
 @Controller('flights')
 export class FlightController {
-    constructor(private readonly flightService: FlightService) {}
+    constructor(
+        private readonly flightService: FlightService,
+        private readonly dataGenerator: DataGenerator,
+    ) {}
 
     @Get('/all')
     async getFlights() {
         return await this.flightService.findAll();
+    }
+    @Get('/:id')
+    async getFlightById(@Param('id') id_flight: number) {
+        return await this.flightService.findOne({ id_flight: id_flight });
     }
 }
--- src/modules/flight/flight.module.ts
+++ src/modules/flight/flight.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { FlightController } from './flight.controller';
 import { FlightService } from '../../shared/services/flight.service';
+import { DataGenerator } from 'src/classes/dataGenerator';
 
 @Module({
     controllers: [FlightController],
-    providers: [FlightService, PrismaService],
+    providers: [FlightService, PrismaService, DataGenerator],
 })
 export class FlightModule {}
